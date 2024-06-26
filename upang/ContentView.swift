46e3aa44fdb7c99e5faf9b17e963a234f9b6f2dd
Alvin Miftah <azraqazravin@gmail.com>
2024-06-26 14:12:47 +0700

    Initial Commit

---
+++ upang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  upang
+//
+//  Created by Alvin Miftah on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ upang/upangApp.swift
@@ -0,0 +1,17 @@
+//
+//  upangApp.swift
+//  upang
+//
+//  Created by Alvin Miftah on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct upangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
